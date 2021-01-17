3f16457473be3652eb1986988126aecc13462c06
wangyacheng <wangyacheng@doffe.com>
2021-01-17 13:47:59 +0800

    1，test

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,5 +1,23 @@
-﻿using System;
+﻿/*********************************
+ *
+ * 文件名(File Name)      Program.cs
+ * 
+ * 功能描述(Description)： 程序入口类
+ * 
+ * 作者(Atuhor)           wangxiaodang
+ * 
+ * 创建(Create Time)      2020/05/12
+ * 
+ * 修改记录(Revision History)
+ * 
+ *      R1:
+ *         修改作者：
+ *         修改日期：
+ *         修改理由：
+ * *******************************/
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +26,16 @@ namespace Code
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+         
         }
+
+
+     
     }
+
+    
 }
